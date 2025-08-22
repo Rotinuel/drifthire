8235edaa1dfed0554784a056c7037133f9b86134
Immanuel Okhuarobo <emmanuelisagenius@gmail.com>
2025-08-22 21:46:36 +0100

    logo update

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import Logo from "../public/logo.png";
 
 export default function Home() {
   return (
     <>
     <nav className="fixed w-full h-24 shadow-xl bg-white">
       <div className="flex justify-between items-center w-full h-full px-4 2xl:px-16">
-        <div className="">left side</div>
+        <Image src={Logo} alt="logo" width="205" height="75" className="cursor-pointer" priority />
         <div className="">Right side</div>
       </div>
     </nav>
