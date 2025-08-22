b2ec32d53532d7d7be9e5977fb6d919037a281bb
Immanuel Okhuarobo <emmanuelisagenius@gmail.com>
2025-08-22 21:01:03 +0100

    first navbar

--- app/page.js
+++ app/page.js
@@ -2,6 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Welcome to Drift Hire</h1>
+    <>
+    <nav className="fixed w-full h-24 shadow-xl bg-white">
+      <div className="flex justify-between items-center w-full h-full px-4 2xl:px-16">
+        <div className="">left side</div>
+        <div className="">Right side</div>
+      </div>
+    </nav>
+    <div className="text-center align-center">Welcome to Drift Hire</div>
+    </>
+    
   );
 }
