712de792e3b15f85d1789f81a0d7a0ab03932be0
Immanuel Okhuarobo <emmanuelisagenius@gmail.com>
2025-08-23 01:41:07 +0100

    large screen navbar

--- app/page.js
+++ app/page.js
@@ -1,17 +1,41 @@
 import Image from "next/image";
 import Logo from "../public/logo.png";
+import Link from "next/link";
 
 export default function Home() {
+  const navList = [
+    {name: "Find Work", href: "#"},
+    {name: "Find Worker", href: "#"},
+    {name: "Services", href: "#"},
+    {name: "Skills Tests", href: "#"},
+    {name: "Blog", href: "#"},
+    {name: "More Info", href: "#"},
+  ]
   return (
     <>
-    <nav className="fixed w-full h-24 shadow-xl bg-white">
-      <div className="flex justify-between items-center w-full h-full px-4 2xl:px-16">
-        <Image src={Logo} alt="logo" width="205" height="75" className="cursor-pointer" priority />
-        <div className="">Right side</div>
-      </div>
-    </nav>
-    <div className="text-center align-center">Welcome to Drift Hire</div>
+      <nav className="fixed w-full h-16 shadow-xl bg-white">
+        <div className="flex justify-between items-center w-full h-full lg:px-28 gap-2">
+          <Link href="/" className="text-2xl font-bold">
+            <Image
+              src={Logo}
+              alt="logo"
+              width="180"
+              height="180"
+              className="cursor-pointer"
+              priority
+            />
+          </Link>
+          <div className="hidden lg:flex gap-16 text-base font-semibold">
+            {navList.map((item) => (
+              <Link key={item.href} href={item.href}>{item.name}</Link>
+            ))}
+          </div>
+          <div className="mr-10">
+            <button className="px-8 py-3 rounded-lg bg-[#439fe4] text-white text-medium font-bold">Sign In</button>
+          </div>
+        </div>
+      </nav>
+      <div className="bg-white mt-120px">hello</div>
     </>
-    
   );
 }
