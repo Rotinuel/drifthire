2c202f6c5faa34a7330e1afb658f7f112ebd27bd
xalphi <emmanuelisagenius@gmail.com>
2025-08-29 16:01:33 +0100

    feeling

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import HireSection from "./components/hireSection";
+import RemoteJobListings from "./components/remoteJobListing";
 
 export default function Home() {
   
   return (
     <>
       <HireSection />
+      <RemoteJobListings />
     </>
   );
 }
